f701b64fcef36bc97ec67096677e0f8f352d7fcd
jeonchangwi1 <jeonchanghwi123@gmail.com>
2024-10-24 10:59:30 +0900

    7주차 실습파일
    7주차 실습파일

---
+++ 7week/ex2.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+df = pd.read_csv('2024_seoul_data.csv', encoding='utf-8')
+df2 = df.fillna(method='ffill')
+df2.info()
+
+df2.rename(columns={'일강수량':'rain'}, inplace=True)
+df2.head(3)
+
+plt.rc('font', family='Malgun Gothic')
+plt.rcParams['axes.unicode_minus'] = False
+
+plt.title('서울시 2024년도 여름 강수량 변화')
+plt.plot(range(1,len(df2)+1), df2['rain'], label='강수량', c='r')
+
+plt.xlabel('일')
+plt.ylabel('강수량')
+plt.legend()
+plt.show()
